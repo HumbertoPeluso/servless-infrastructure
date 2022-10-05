a853026dd4059e0b4e574b0f6daa55cfa81299bf
Humberto <humberto.peluso@yahoo.com.br>
2022-10-05 12:40:40 -0300

    First commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "1.3.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+  #profile = var.aws_profile
+
+  default_tags {
+    tags = {
+      Project   = "Serverless REST API"
+      CreatedAt = "2022-09-26"
+      ManagedBy = "Terraform"
+      Owner     = "Humberto Peluso"
+      Env       = var.env
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+/* variable "aws_profile" {
+  type    = string
+  default = "tf014"
+} */
+
+variable "aws_account_id" {
+  type    = string
+}
+
+variable "service_name" {
+  type    = string
+  default = "todos"
+}
